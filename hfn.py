ff07d58034d2d04c3b7a66fcaeb963f126574525
Dan <149019458+typtrue@users.noreply.github.com>
2024-05-29 11:37:35 +0100

    fix complex

--- hfn.py
+++ hfn.py
@@ -51,62 +51,62 @@ class HopfieldComplex:
         self.ishape = (np.prod(self.shape), np.prod(self.shape)) if interactions == None else interactions.shape
         self.interactions = np.zeros(self.ishape, dtype=np.float16) if interactions == None else interactions
 
-    # def update_async(self, states):
-    #     interactions = self.interactions
-    #     newstates = states.copy()
-    #     for i in range(len(states)):
-    #         avg = np.dot(states,interactions[i,].T)
-    #         newstates[i] = np.exp(1j*np.angle(avg))
-    #         states = newstates.copy()
-    #     return states
-
-    # def update_sync(self, states):
-    #     interactions = self.interactions
-    #     newstates = np.zeros(np.prod(self.shape), dtype=np.complex64)
-    #     for i in range(len(states)):
-    #         avg = np.dot(states,interactions[i,].T)
-    #         newstates[i] = np.complex64(np.exp(1j*np.angle(avg)))
-    #     return newstates
-
-    # def train_network(self, states):
-    #     self.savedimages += 1
-    #     n = self.savedimages
-    #     traindata = np.outer(states, (1/states).T)
-    #     traindata = 1 - abs(np.angle(traindata)) * 2 / np.pi
-    #     np.fill_diagonal(traindata, 0)
-
-    #     self.interactions = ((n-1) * self.interactions + traindata) / n
-
     def update_async(self, states):
         interactions = self.interactions
         newstates = states.copy()
         for i in range(len(states)):
-            avg = np.dot(states,interactions[i,].T) / states.size
-            newstates[i] = avg
+            avg = np.dot(states,interactions[i,].T)
+            newstates[i] = np.exp(1j*np.angle(avg))
             states = newstates.copy()
         return states
 
     def update_sync(self, states):
         interactions = self.interactions
-        newstates = np.zeros(np.prod(self.shape), dtype=np.float32)
+        newstates = np.zeros(np.prod(self.shape), dtype=np.complex64)
         for i in range(len(states)):
-            avg = np.dot(states,interactions[i,].T) / states.size
-            print(avg)
-            newstates[i] = avg
+            avg = np.dot(states,interactions[i,].T)
+            newstates[i] = np.complex64(np.exp(1j*np.angle(avg)))
         return newstates
 
     def train_network(self, states):
         self.savedimages += 1
         n = self.savedimages
-        k = np.tile(states, (states.size, 1))
-        traindata = 1 - abs(k - k.T)
-
+        traindata = np.outer(states, (1/states).T)
+        traindata = 1 - abs(np.angle(traindata)) * 2 / np.pi
         np.fill_diagonal(traindata, 0)
 
-
-        a = np.ones((states.size,)) - np.identity(states.size)
-
         self.interactions = ((n-1) * self.interactions + traindata) / n
+
+    # def update_async(self, states):
+    #     interactions = self.interactions
+    #     newstates = states.copy()
+    #     for i in range(len(states)):
+    #         avg = np.dot(states,interactions[i,].T) / states.size
+    #         newstates[i] = avg
+    #         states = newstates.copy()
+    #     return states
+
+    # def update_sync(self, states):
+    #     interactions = self.interactions
+    #     newstates = np.zeros(np.prod(self.shape), dtype=np.float32)
+    #     for i in range(len(states)):
+    #         avg = np.dot(states,interactions[i,].T) / states.size
+    #         print(avg)
+    #         newstates[i] = avg
+    #     return newstates
+
+    # def train_network(self, states):
+    #     self.savedimages += 1
+    #     n = self.savedimages
+    #     k = np.tile(states, (states.size, 1))
+    #     traindata = 1 - abs(k - k.T)
+
+    #     np.fill_diagonal(traindata, 0)
+
+
+    #     a = np.ones((states.size,)) - np.identity(states.size)
+
+    #     self.interactions = ((n-1) * self.interactions + traindata) / n
 
     def energy_function(self, states):
         return -((self.interactions @ states) @ states)
